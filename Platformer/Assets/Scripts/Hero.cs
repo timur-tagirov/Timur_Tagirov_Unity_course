de120c0549a3daadbf6c0a1238195284ade0e130
timur-tagirov <109279553+timur-tagirov@users.noreply.github.com>
2022-07-24 23:58:10 +0300

    2_Оживляем_сцену

---
+++ Platformer/Assets/Scripts/Hero.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+
+{
+    [SerializeField]
+
+    private float _speed;
+
+    private float _direction;
+
+    public void SetDirection(float direction)
+    {
+        _direction = direction;
+    }
+
+    
+    private void Update()
+    {
+
+        if (_direction != 0)
+        {
+            var delta = _direction * _speed * Time.deltaTime;
+            var newXPosition = transform.position.x + delta;
+            var newYPosition = transform.position.y + delta;
+            transform.position = new Vector3(newXPosition, newYPosition, transform.position.z);
+        }
+
+
+    }
+
+    public void SaySomething()
+    {
+        Debug.Log("Something!");
+    }
+}
---
+++ Platformer/Assets/Scripts/HeroInputReader.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HeroInputReader : MonoBehaviour
+{
+
+   [SerializeField] private Hero _hero;
+
+
+    public void OnMovement(InputAction.CallbackContext context)
+    {
+        var direction = context.ReadValue<float>();
+        _hero.SetDirection(direction);
+
+    }
+
+    
+
+
+    public void OnSaySomething(InputAction.CallbackContext context)
+    {
+        if (context.canceled)
+        {
+            _hero.SaySomething();
+        }
+        
+    }
+
+}
