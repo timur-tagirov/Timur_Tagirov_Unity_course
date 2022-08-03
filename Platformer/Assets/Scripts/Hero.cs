d00433b2fc81be2de5bf87ab72f6f46b9d74a924
timur-tagirov <109279553+timur-tagirov@users.noreply.github.com>
2022-08-03 22:31:01 +0300

    2.3 Физика и коллизии
    Работа над ошибками.

--- Platformer/Assets/Scripts/Hero.cs
+++ Platformer/Assets/Scripts/Hero.cs
@@ -7,9 +7,8 @@ public class Hero : MonoBehaviour
 {
     [SerializeField] private float _speed;
     [SerializeField] private float _jumpSpeed;
-    [SerializeField] private LayerMask _groundLayer;
-    [SerializeField] private float _groundCheckRadius;
-    [SerializeField] private Vector3 _groundCheckPositionDelta;
+
+    [SerializeField] private LayerCheck _groundCheck;
 
     private Rigidbody2D _rigidbody;
     private Vector2 _direction;
@@ -34,7 +33,7 @@ public class Hero : MonoBehaviour
             if (IsGrounded())
             {
                 _rigidbody.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
-            }            
+            }
         } else if (_rigidbody.velocity.y > 0)
         {
             _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.y * 0.5f);
@@ -43,14 +42,7 @@ public class Hero : MonoBehaviour
 
     private bool IsGrounded()
     {
-        var hit = Physics2D.CircleCast(transform.position + _groundCheckPositionDelta, _groundCheckRadius, Vector2.down, 0, _groundLayer);
-        return hit.collider != null;
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = IsGrounded() ? Color.green : Color.red;
-        Gizmos.DrawSphere(transform.position + _groundCheckPositionDelta, _groundCheckRadius);
+        return _groundCheck.IsTouchingLayer;
     }
 
     public void SaySomething()
