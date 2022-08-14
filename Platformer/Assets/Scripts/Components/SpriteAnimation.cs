27fbd2fb3e14825b218e92752540622336ad4667
timur-tagirov <109279553+timur-tagirov@users.noreply.github.com>
2022-08-14 17:47:43 +0300

    2.4 Анимация

---
+++ Platformer/Assets/Scripts/Components/SpriteAnimation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace PixelCrew
+{ 
+    [RequireComponent(typeof(SpriteRenderer))]
+
+    public class SpriteAnimation : MonoBehaviour
+    {
+        [SerializeField] private int _frameRate;
+        [SerializeField] private bool _loop;
+        [SerializeField] private Sprite[] _sprites;
+        [SerializeField] private UnityEventQueueSystem _onComplete;
+
+        private SpriteRenderer _renderer;
+        private float _secondsPerFrame;
+        private int _currentSpriteIndex;
+        private float _nextFrameTime;
+        private bool _isPlaying = true;
+
+        private void Start()
+        {
+            _renderer = GetComponent<SpriteRenderer>();
+            _secondsPerFrame = 1f / _frameRate;
+            _nextFrameTime = Time.time + _secondsPerFrame;
+        }
+
+        private void Update()
+        {
+            if (!_isPlaying || _nextFrameTime > Time.time) return;
+            if (_currentSpriteIndex >= _sprites.Length)
+            {
+                if (_loop)
+                {
+                    _currentSpriteIndex = 0;
+                }
+                else
+                {
+                    _isPlaying = false;
+                    return;
+                }
+            }
+            _renderer.sprite = _sprites[_currentSpriteIndex];
+            _nextFrameTime += _secondsPerFrame;
+            _currentSpriteIndex++;
+        }
+    }
+}
--- Platformer/Assets/Scripts/Hero.cs
+++ Platformer/Assets/Scripts/Hero.cs
@@ -11,10 +11,18 @@ using UnityEngine;
 
         private Rigidbody2D _rigidbody;
         private Vector2 _direction;
+        private Animator _animator;
+        private SpriteRenderer _sprite;
 
-        private void Awake()
+        private static readonly int IsGroundKey = Animator.StringToHash("is-ground");
+        private static readonly int IsRunning = Animator.StringToHash("is-running");
+        private static readonly int VerticalVelocity = Animator.StringToHash("vertical-velocity");
+
+    private void Awake()
         {
             _rigidbody = GetComponent<Rigidbody2D>();
+            _animator = GetComponent<Animator>();
+            _sprite = GetComponent<SpriteRenderer>();
         }
 
         public void SetDirection(Vector2 direction)
@@ -27,9 +35,10 @@ using UnityEngine;
             _rigidbody.velocity = new Vector2(_direction.x * _speed, _rigidbody.velocity.y);
 
             var isJumping = _direction.y > 0;
+            var isGrounded = IsGrounded();
             if (isJumping)
             {
-                if (IsGrounded())
+                if (isGrounded && _rigidbody.velocity.y <=0)
                 {
                     _rigidbody.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
                 }
@@ -38,7 +47,25 @@ using UnityEngine;
             {
                 _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.y * 0.5f);
             }
+
+        _animator.SetBool(IsGroundKey, isGrounded);
+        _animator.SetBool(IsRunning, _direction.x != 0);
+        _animator.SetFloat(VerticalVelocity, _rigidbody.velocity.y);
+
+        UpdateSpriteDirection();
+    }
+
+    private void UpdateSpriteDirection()
+    {
+        if (_direction.x > 0)
+        {
+            _sprite.flipX = false;
         }
+        else if (_direction.x < 0)
+        {
+            _sprite.flipX = true;
+        }
+    }
 
         private bool IsGrounded()
         {
